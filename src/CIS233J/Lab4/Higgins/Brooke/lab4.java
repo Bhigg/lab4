b60f07b739ba637d5d105d2589d2f2519b0a0497
Brooke Higgins <brooke.higgins@pcc.edu>
2017-05-02 17:27:52 -0700

    added main method that prints a message

--- src/CIS233J/Lab4/Higgins/Brooke/lab4.java
+++ src/CIS233J/Lab4/Higgins/Brooke/lab4.java
@@ -1,7 +1,10 @@
 package CIS233J.Lab4.Higgins.Brooke;
 
 /**
- * Created by Brooke on 5/2/2017.
+ * Created by Brooke Higgins on 5/2/2017.
  */
 public class lab4 {
+    public static void main(String[] args){
+        System.out.println("I successfully created a repository for assignment number 4.");
+    }
 }
